cfaecd8e4d45664c3962413e1435b1436c9e7535
treeshay <shayleighton24@gmail.com>
2026-03-24 15:20:10 +0000

    current nav link styling

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,4 @@
-<a href="{{ $href }}">{{ $slot }}</a>
+@props(['active' => false])
+<a class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium text-white"
+{{ $attributes }}
+>{{ $slot }}</a>
