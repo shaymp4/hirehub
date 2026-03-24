f12ca65082f993e3df6e7c49b42b64e157f4e21f
treeshay <shayleighton24@gmail.com>
2026-03-24 16:16:27 +0000

    created and implemented Job class

---
+++ app/Models/Job.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Support\Arr;
+
+class Job
+{
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Software Engineer',
+                'salary' => '$90,000 - $120,000',
+                'description' => 'We are looking for a skilled Software Engineer to join our team. You will be responsible for developing high-quality applications and collaborating with cross-functional teams.',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Product Manager',
+                'salary' => '$100,000 - $140,000',
+                'description' => 'We are seeking an experienced Product Manager to lead the development and launch of our new products. You will work closely with engineering, design, and marketing teams to ensure successful product delivery.',
+            ],
+            [
+                'id' => 3,
+                'title' => 'UX Designer',
+                'salary' => '$80,000 - $110,000',
+                'description' => 'We are in need of a creative UX Designer to help us design intuitive and engaging user experiences. You will collaborate with product managers and developers to create user-centered designs.',
+            ],
+        ];
+    }
+
+    public static function find($id)
+    {
+        return Arr::first(static::all(), fn ($job) => $job['id'] == $id);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('home');
@@ -12,50 +14,12 @@ Route::get('/about', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '$90,000 - $120,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Product Manager',
-                'salary' => '$100,000 - $140,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'UX Designer',
-                'salary' => '$80,000 - $110,000',
-            ],
-        ],
+    return view('jobs', ['jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-            [
-                'id' => 1,
-                'title' => 'Software Engineer',
-                'salary' => '$90,000 - $120,000',
-                'description' => 'We are looking for a skilled Software Engineer to join our team. You will be responsible for developing high-quality applications and collaborating with cross-functional teams.',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Product Manager',
-                'salary' => '$100,000 - $140,000',
-                'description' => 'We are seeking an experienced Product Manager to lead the development and launch of our new products. You will work closely with engineering, design, and marketing teams to ensure successful product delivery.',
-            ],
-            [
-                'id' => 3,
-                'title' => 'UX Designer',
-                'salary' => '$80,000 - $110,000',
-                'description' => 'We are in need of a creative UX Designer to help us design intuitive and engaging user experiences. You will collaborate with product managers and developers to create user-centered designs.',
-            ],
-        ];
-
-        $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+    $job = Job::find($id);
 
     return view('job', ['job' => $job]);
 });
